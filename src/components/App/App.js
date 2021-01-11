ad8087b9f05bf183577e8fde00f5d0b5a1796bbf
Yurii Berezynets <yuriiberezynets@gmail.com>
2021-01-11 10:24:47 +0200

    transfers add to components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import styles from "./App.module.css";
-import Profile from "./components/Profile/Profile";
-import user from "./data/user.json";
-import Statistics from "./components/Statistics/Statistics";
-import statisticalData from "./data/statistical-data.json";
-import FriendList from "./components/FriendList/FriendList";
-import friends from "./data/friends.json";
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
-import transactions from "./data/transactions.json";
+import Profile from "../Profile/Profile";
+import user from "../../data/user.json";
+import Statistics from "../Statistics/Statistics";
+import statisticalData from "../../data/statistical-data.json";
+import FriendList from "../FriendList/FriendList";
+import friends from "../../data/friends.json";
+import TransactionHistory from "../TransactionHistory/TransactionHistory";
+import transactions from "../../data/transactions.json";
 
 function App() {
   return (
